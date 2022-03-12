c0c665ac4560e1173acd06398cde8f190d0c7005
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-12 11:59:35 -0300

    commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,6 +6,16 @@ const x = gritar('bom dia')
 
 console.log(x)
 
-function obterCotacaoDolarHoje() {
-    
+function somar(a = 10, b = 10){
+    return a + b
 }
+
+const result = somar(3, 5, 6, 7, 8, 9, 10)
+console.log(result)
+
+const result2 = somar(3)
+console.log(result2)
+
+const result3 = somar()
+console.log(result3)
+
