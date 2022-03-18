8ea6dc49798d8ccea134a50779dea3b2efb77c74
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-18 17:28:54 -0300

    commit

---
+++ JsDAM/script.js
@@ -0,0 +1,10 @@
+var nota1 = 4;
+var nota2 = 7;
+var nota3= 10;
+var media = (nota1+nota2+nota3)/2;
+
+if(media >= 7) {
+    alert(`Sua medía é ${media}, APROVADO! :)`);
+}else {
+    alert(`Sua média é ${media}, REPROVADO! :(`);
+}
