65d5eaf6f9de7815f9d8f8bb2d829b2468d16ab4
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-03-20 12:56:20 -0300

    commit

---
+++ JavaScri/script.js
@@ -0,0 +1,9 @@
+function carreguei() {
+    document.getElementsById('logo')
+    .innerHTML += "Carreguei<br>"
+}
+
+function redimesionei() {
+    document.getElementsById('logo') 
+    .innerHTML += "redimesionei<br>"
+}
